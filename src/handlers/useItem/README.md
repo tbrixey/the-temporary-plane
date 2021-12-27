b5473614636010b4ce482c3ece834e6da75dcf4b
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2021-12-26 18:18:24 -0600

    Current consumables all work now.

--- src/handlers/useItem/README.md
+++ src/handlers/useItem/README.md
@@ -13,3 +13,5 @@ Checks that the player has at least 1 of those items in their bag then uses that
   "message": "string"
 }
 ```
+
+Right now there is a limit of 1 bonus stat item at a time. Maybe later I will allow stacking of stat boosting items. Anything that alters a player stat is a stat bonus item. Equipment is separate to items. For example to get 3 bonus Intelligence, Equip Mage Robes and drink an Intelligence Potion.
--- src/handlers/useItem/useItem.ts
+++ src/handlers/useItem/useItem.ts
@@ -1,5 +1,6 @@
 import { Response } from "express";
 import { find, findIndex } from "lodash";
+import moment from "moment";
 import { client, dbName } from "../../mongo";
 import { ExpressRequest } from "../../types/express";
 
@@ -23,8 +24,7 @@ export const useItem = async (req: ExpressRequest, res: Response) => {
     if (currentUser.bag[itemIndex].type === "consumable") {
       if (currentUser.bag[itemIndex].effect) {
         const effect = Object.keys(currentUser.bag[itemIndex].effect);
-
-        console.log("EFFECT", effect);
+        const now = new Date();
 
         switch (effect[0]) {
           case "hitpoints":
@@ -53,7 +53,7 @@ export const useItem = async (req: ExpressRequest, res: Response) => {
             } else {
               return res
                 .status(200)
-                .json({ message: "Player is already at max health" });
+                .json({ message: "You are already at max health" });
             }
             break;
           case "stats":
@@ -62,25 +62,71 @@ export const useItem = async (req: ExpressRequest, res: Response) => {
               "bag.$[elem].count": -1,
             };
             arrayFilters.arrayFilters = [{ "elem.id": itemId }];
+            setField.$set = {
+              bonusStats: {
+                stats: statItem(currentUser.bag[itemIndex].effect.stats),
+                time: moment(now)
+                  .add(currentUser.bag[itemIndex].effect.time, "m")
+                  .toDate(),
+              },
+            };
+            break;
+          case "speed":
+          case "weight":
+            const key = Object.keys(currentUser.bag[itemIndex].effect);
+            setField.$inc = {
+              "bag.$[elem].count": -1,
+            };
+            arrayFilters.arrayFilters = [{ "elem.id": itemId }];
+            setField.$set = {
+              bonusStats:
+                key[0] === "speed"
+                  ? { speed: currentUser.bag[itemIndex].effect.speed }
+                  : { weight: currentUser.bag[itemIndex].effect.weight },
+            };
+            setField.$set.bonusStats.time = moment(now)
+              .add(currentUser.bag[itemIndex].effect.time, "m")
+              .toDate();
             break;
         }
       }
+    } else if (currentUser.bag[itemIndex].type === "junk") {
+      return res
+        .status(200)
+        .json({ message: "You can't use " + currentUser.bag[itemIndex].name });
     }
 
-    // const updateResponse = await playerCollection.findOneAndUpdate(
-    //   {
-    //     apiKey: currentUser.apiKey,
-    //   },
-    //   setField,
-    //   arrayFilters
-    // );
+    if (setField.isEmpty) {
+      await playerCollection.updateOne(
+        {
+          apiKey: currentUser.apiKey,
+        },
+        setField,
+        arrayFilters
+      );
+    }
 
     return res
       .status(200)
-      .json({ message: "Player used " + currentUser.bag[itemIndex].name });
+      .json({ message: "You used " + currentUser.bag[itemIndex].name });
   } else {
-    return res
-      .status(400)
-      .json({ message: "Player does not have item to use." });
+    return res.status(400).json({ message: "You does not have item to use." });
+  }
+};
+
+const statItem = (stat: { [key: string]: number }) => {
+  const key = Object.keys(stat);
+
+  switch (key[0]) {
+    case "str":
+      return { str: stat.str };
+    case "con":
+      return { con: stat.con };
+    case "dex":
+      return { dex: stat.dex };
+    case "int":
+      return { int: stat.int };
+    case "luck":
+      return { luck: stat.luck };
   }
 };
