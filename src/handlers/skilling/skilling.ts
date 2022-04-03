a476e9466996abfbd192e941d3a91b9bb0df2c2e
Trevor Brixey <33526666+tbrixey@users.noreply.github.com>
2022-04-03 14:54:24 -0500

    skilling updates to get finish time.

--- src/handlers/skilling/skilling.ts
+++ src/handlers/skilling/skilling.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import moment from "moment";
 import { client, dbName } from "../../mongo";
 import { ExpressRequest } from "../../types";
 
@@ -14,13 +15,20 @@ export const skilling = async (req: ExpressRequest, res: Response) => {
   const skill = req.params.skillName;
   const itemToGet = req.params.item;
 
+  if (!skill || !itemToGet) {
+    return res.status(400).json({
+      message: `invalid request. Missing either /:skillName or /:item`,
+    });
+  }
+
   const findObj: FindObject = {
     location: currentUser.location,
     skill,
     level: { $lte: currentUser.level },
+    itemName: itemToGet.toLowerCase(),
   };
 
-  if (itemToGet) findObj.itemName = itemToGet;
+  console.log(findObj);
 
   const collection = client.db(dbName).collection("skills");
   const skills = await collection.find(findObj).toArray();
@@ -32,6 +40,18 @@ export const skilling = async (req: ExpressRequest, res: Response) => {
       .status(200)
       .json({ message: `Unable to work on ${skill} here.` });
   }
+
+  const now = new Date();
+  const finishTime = moment(now).add(skills[0].time, "s").toDate();
+
+  const skillingCollection = client.db(dbName).collection("skilling");
+
+  // await skillingCollection.insertOne({
+  //   playerName: currentUser.playerName,
+  //   skill,
+  //   finishTime,
+  //   count,
+  // });
 
   res.status(200).json({
     message: `Started working on ${skill} getting ${itemToGet}`,
